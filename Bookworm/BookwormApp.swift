b819c4891c061a1c6c1f2621e37e35f870defc7b
Joel Lacerda <joellacerdaol@gmail.com>
2025-03-13 17:49:11 -0300

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Joel Lacerda on 13/03/25.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
